2626bed5e92ec10c7f112a5d35bf1707b3020667
Loïc Bourgois <loicbourgois.web@gmail.com>
2016-01-30 08:54:57 +0100

    Commit.

---
+++ home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link type="text/css" rel="stylesheet" href="./css/home.css" />
+		<script type="text/javascript" src="./js/home.js"></script>
+		<title>Gestion d'équipe - Acceuil</title>
+	</head>
+	<body>
+		<p>
+			Paragraphe
+		</p>
+	</body>
+</html>
+
+<?php
+	//echo("Hey");
+?>
